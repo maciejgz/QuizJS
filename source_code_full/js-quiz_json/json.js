a81487bb167c57adbd089d747933f7a268813cac
smieci000 <smieci000@gmail.com>
2015-09-20 23:10:01 +0200

    wersja przed podświetlaniem buttonów

---
+++ source_code_full/js-quiz_json/json.js
@@ -0,0 +1,10 @@
+// JSON Document
+data = {"quizcontent": [
+        {"question": "question text 1", "image": "1.jpg", "correct": 1, "a1":"answer 1", "a2":"answer 2","a3":"answer 3","a4":"answer 4"},
+    {"question": "question text 2", "image": "2.jpg", "correct": 3, "a1":"answer 1", "a2":"answer 2","a3":"answer 3","a4":"answer 4"},
+	{"question": "question text 3", "image": "3.jpg", "correct": 4, "a1":"answer 1", "a2":"answer 2","a3":"answer 3","a4":"answer 4"},
+	{"question": "question text 4", "image": "4.jpg", "correct": 4, "a1":"answer 1", "a2":"answer 2","a3":"answer 3","a4":"answer 4"}
+    ],
+	
+};
+
